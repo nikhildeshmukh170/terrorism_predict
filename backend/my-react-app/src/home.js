3fdc486291d0a7a39a932da99721a98cf8b83dba
Nikhil Deshmukh <115735153+nikhildeshmukh170@users.noreply.github.com>
2025-02-07 20:15:51 +0530

    update this

--- backend/my-react-app/src/home.js
+++ backend/my-react-app/src/home.js
@@ -1,17 +1,24 @@
-// Home.js
-
-
 import React from 'react';
-import './home.css'; // Import the CSS file for styling
+import './home.css';
 
-const home = () => {
+const Home = () => {
   return (
     <div className="home-container">
       <h1>Welcome to the Home Page</h1>
-      <p>Here is some content...</p>
-      <iframe title="hackskill" width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiYmQ0OThlNzAtNTQ5OS00MzQ2LWExY2UtOGFhZGZkNDI1YTdkIiwidCI6ImRmODY3OWNkLWE4MGUtNDVkOC05OWFjLWM4M2VkN2ZmOTVhMCJ9" frameborder="0" allowFullScreen="true"></iframe>
-    </div>
-  )
-}
+      <p>Explore insights with our interactive dashboard.</p>
 
-export default home
+      <div className="iframe-container">
+        <iframe 
+          title="hackskill" 
+          width="100%" 
+          height="100%" 
+          src="https://app.powerbi.com/view?r=eyJrIjoiYmQ0OThlNzAtNTQ5OS00MzQ2LWExY2UtOGFhZGZkNDI1YTdkIiwidCI6ImRmODY3OWNkLWE4MGUtNDVkOC05OWFjLWM4M2VkN2ZmOTVhMCJ9" 
+          frameBorder="0" 
+          allowFullScreen
+        ></iframe>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
